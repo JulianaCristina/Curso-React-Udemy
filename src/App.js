5470b8ae1b30da0414d76c1315f63aced18dbd44
juliana <julianacristina780@gmail.com>
2020-05-15 22:14:35 -0300

    Using Radium for media queries

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
-import Radium from "radium";
+import Radium, {StyleRoot} from "radium";
 import Person from './Person/Person';
 
 class App extends Component {
     state = {
         persons: [
             {id: '1', name: 'Max', age: 28},
-            {id: '2',name: 'Manu', age: 29},
-            {id: '3',name: 'Stephanie', age: 26}
+            {id: '2', name: 'Manu', age: 29},
+            {id: '3', name: 'Stephanie', age: 26}
         ],
         otherState: 'some other value',
         showPersons: false
@@ -29,7 +29,7 @@ class App extends Component {
         const persons = [...this.state.persons];
         persons[personIndex] = person;
 
-        this.setState({persons:persons})
+        this.setState({persons: persons})
     }
 
     deletePersonHandler = (personIndex) => {
@@ -65,7 +65,7 @@ class App extends Component {
                 <div>
                     {this.state.persons.map((person, index) => {
                         return <Person
-                            click={ () => this.deletePersonHandler(index)}
+                            click={() => this.deletePersonHandler(index)}
                             name={person.name}
                             age={person.age}
                             key={person.id}
@@ -75,34 +75,36 @@ class App extends Component {
                 </div>
             );
             style.backgroundColor = 'red';
-            style[':hover'] =  {
+            style[':hover'] = {
                 backgroundColor: 'salmon',
-                    color: 'black'
+                color: 'black'
             }
         }
 
-       // const classes = ['red', 'bold'].join(' '); //"red bold"
+        // const classes = ['red', 'bold'].join(' '); //"red bold"
 
         const classes = []
 
-        if (this.state.persons.length <=2){
+        if (this.state.persons.length <= 2) {
             classes.push('red') //classes = ['red']
         }
-        if (this.state.persons.length <=1) {
+        if (this.state.persons.length <= 1) {
             classes.push('bold'); //classes = ['red', 'bold']
         }
 
         return (
-            <div className="App">
-                <h1>Hi, I'm a React App</h1>
-                {/*<p className={classes}>This is really working!</p>*/}
-                <p className={classes.join(' ')}>This is really working!</p>
-                <button
-                    style={style}
-                    onClick={this.togglePersonsHandler}>Toggle Persons
-                </button>
-                {persons}
-            </div>
+            <StyleRoot>
+                <div className="App">
+                    <h1>Hi, I'm a React App</h1>
+                    {/*<p className={classes}>This is really working!</p>*/}
+                    <p className={classes.join(' ')}>This is really working!</p>
+                    <button
+                        style={style}
+                        onClick={this.togglePersonsHandler}>Toggle Persons
+                    </button>
+                    {persons}
+                </div>
+            </StyleRoot>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
     }
